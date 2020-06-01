64df50ee7cc9a644a76be31f840879c1e24019a0
Mehran Kader <mehran@misfit.tech>
2020-06-02 00:23:55 +0600

    add: support for go mod with main function

---
+++ TestGo/code/code.go
@@ -0,0 +1,40 @@
+package code
+
+import (
+	"strconv"
+)
+
+// TestCode : Function Imported in main.go
+func TestCode(year int) (date string) {
+	isLeap := false
+	if (
+		(year < 1918 && year % 4 == 0) || 
+		(year % 4 == 0 && year % 100 != 0) || 
+		(year % 400 == 0)) {
+		isLeap = true
+	}
+	
+	// month := 9
+	baseDays := 243
+	if year == 1918 {
+		baseDays = 230
+	}
+	countDays := 256
+	if isLeap {
+		countDays = 255
+	}
+	day := (countDays - baseDays)
+
+	date = strconv.Itoa(day) + ".09." + strconv.Itoa(year)
+	return date
+
+}
+
+// package main
+// import (
+// 	"fmt"
+// 	"strconv"
+// )
+// func main() {
+// 	fmt.Println(ProgrammersDay(2017))
+// }
---
+++ TestGo/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"code/code"
+)
+
+
+func main() {
+	fmt.Println(code.ProgrammersDay(2017))
+}
